50ccb0af96db2987cba4b50da6eeeacbd4e18768
MGSimard <mgsimard.dev@gmail.com>
2025-04-24 08:05:40 -0400

    React Compiler

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,20 @@ import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 
+const ReactCompilerConfig = {
+  target: "19",
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact()],
+  plugins: [
+    TanStackRouterVite(),
+    viteReact({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
