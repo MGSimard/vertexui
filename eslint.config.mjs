1da8ed2afab29d6d0ed30a7da1d8a08ad5c82081
MGSimard <mgsimard.dev@gmail.com>
2025-04-24 08:03:17 -0400

    React Compiler

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,48 @@
 import tseslint from "typescript-eslint";
 import tsParser from "@typescript-eslint/parser";
+import * as reactHooks from "eslint-plugin-react-hooks";
 
-export default tseslint.config(tseslint.configs.recommendedTypeChecked, tseslint.configs.stylisticTypeChecked, {
-  plugins: {},
-  languageOptions: {
-    parser: tsParser,
-    ecmaVersion: "latest",
-    sourceType: "module",
-    parserOptions: {
-      projectService: true,
-      tsconfigRootDir: import.meta.dirname,
+export default tseslint.config(
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  reactHooks.configs.recommended,
+  {
+    plugins: {},
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-  },
-  rules: {
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        argsIgnorePattern: "^_",
-      },
-    ],
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        checksVoidReturn: {
-          attributes: false,
+    rules: {
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
         },
-      },
-    ],
-  },
-});
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
+      "react-hooks/react-compiler": "error",
+    },
+  }
+);
