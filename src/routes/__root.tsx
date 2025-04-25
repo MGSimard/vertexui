e2d3751d0850cc9f6444454f44570b44c4b1e222
MGSimard <mgsimard.dev@gmail.com>
2025-04-24 20:14:43 -0400

    a

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,12 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { Sidenav } from "@/_components/Sidenav/Sidenav";
+import { Header } from "@/_components/Header/Header";
 import { Footer } from "@/_components/Footer/Footer";
-
 export const Route = createRootRoute({
   component: () => (
     <>
       <Sidenav />
-      <header></header>
+      <Header />
       <main>
         <Outlet />
       </main>
